1fc01a91441ca8f79d012db274a3b0768a3f2c35
Urip <uripsoft@gmail.com>
2016-11-07 00:17:08 +0500

    Initial Commit

---
+++ lessy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lessy
+//
+//  Created by Михаил Коновалов on 07.11.16.
+//  Copyright © 2016 Михаил Коновалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
